6fb60fcff760d94e4cdfe1a0ba819bebd4614930
Lee Hak Do <dev.fringe@gmail.com>
2020-06-23 14:02:42 +0900

    commit

---
+++ src/main/java/dev/fringe/oauth2/config/OAuth2SecurityConfiguration.java
@@ -0,0 +1,99 @@
+package dev.fringe.oauth2.config;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import dev.fringe.oauth2.model.User;
+import dev.fringe.oauth2.service.ClientAndUserDetailsService;
+
+@Configuration
+public class OAuth2SecurityConfiguration {
+
+	public static final String CLIENT = "test";
+	public static final String SECRET = "test";
+
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Bean
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+			return super.authenticationManagerBean();
+		}
+
+        @Autowired private UserDetailsService userDetailsService;
+
+        @Autowired
+        protected void registerAuthentication(final AuthenticationManagerBuilder auth)
+                throws Exception {
+            auth.userDetailsService(userDetailsService);
+        }
+	}
+	
+	@Configuration
+	@EnableResourceServer
+	public class ResourceServer extends ResourceServerConfigurerAdapter {
+
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().antMatchers("/oauth/token").anonymous();
+            http.authorizeRequests().antMatchers("/customers/**").access("#oauth2.hasScope('write')");
+        }
+	}
+	@Configuration
+	@EnableAuthorizationServer
+    @Order(Ordered.LOWEST_PRECEDENCE - 100)
+	public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+		@Autowired AuthenticationManager authenticationManagerBean;
+		
+        private ClientAndUserDetailsService combinedService;
+		
+        private final List<UserDetails> users = Arrays.asList(User.create("bill", "abc123", "USER"),User.create("bob", "abc123", "USER"),User.create("sophia", "three", "NONE"),User.create("olivia", "four", "USER"));
+        
+		public AuthorizationServer() throws Exception {
+            ClientDetailsService csvc = new InMemoryClientDetailsServiceBuilder().withClient(CLIENT).authorizedGrantTypes("password").authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust").secret(SECRET).accessTokenValiditySeconds(3600).and().build();
+            UserDetailsService svc = new InMemoryUserDetailsManager(users);
+            this.combinedService = new ClientAndUserDetailsService(csvc, svc);
+		}
+
+        @Bean
+        public ClientDetailsService clientDetailsService() throws Exception {
+            return combinedService;
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return combinedService;
+        }
+        
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			endpoints.authenticationManager(authenticationManagerBean);
+		}
+
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.withClientDetails(clientDetailsService());
+        }
+	}
+
+}
---
+++ src/main/java/dev/fringe/oauth2/service/ClientAndUserDetailsService.java
@@ -0,0 +1,45 @@
+package dev.fringe.oauth2.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.client.ClientDetailsUserDetailsService;
+
+/**
+ * A class that combines a UserDetailsService and ClientDetailsService into a
+ * single object.
+ * 
+ * @author jules
+ * 
+ * This code was provided by Jules White.
+ */
+public class ClientAndUserDetailsService implements UserDetailsService, ClientDetailsService {
+
+    private final ClientDetailsService clients;
+    private final UserDetailsService users;
+    private final ClientDetailsUserDetailsService clientDetailsWrapper;
+
+    public ClientAndUserDetailsService(ClientDetailsService clients, UserDetailsService users) {
+        this.clients = clients;
+        this.users = users;
+        this.clientDetailsWrapper = new ClientDetailsUserDetailsService(clients);
+    }
+
+    public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
+        return clients.loadClientByClientId(clientId);
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails user = null;
+        try {
+            user = users.loadUserByUsername(username);
+        } catch (UsernameNotFoundException e) {
+            user = clientDetailsWrapper.loadUserByUsername(username);
+        }
+        return user;
+    }
+
+}
